a08aff17cd6ef429c635c4250ceadc82491074d1
AKASH YADAV <akashgudise21@gmail.com>
2021-01-28 16:07:25 +0530

    Database creation and schema

---
+++ apprenticeship-team-d-master/mongo.js
@@ -0,0 +1,17 @@
+// Create a database and collections in mongodb
+
+
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/Instaclone"; // database name 
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+const db = mongoose.connection; // here  connected
+
+db.once("open", (_) => {        // if connected
+  console.log("Database connected:", url);
+});
+
+db.on("error", (err) => {  // if error
+  console.error("connection error:", err);
+});
