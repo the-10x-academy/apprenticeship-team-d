69e73dcf604269d3c7bf87da8ae0a23720417fbb
Nisha0409 <77746478+Nisha0409@users.noreply.github.com>
2021-01-30 22:05:55 +0530

    Add files via upload (#8)

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const mongo = require("../mongo");
+const postSchema = require("../models/postSchema")
+
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+	res.render("index", { title: "Express" });
+});
+
+
+router.get("/posts", async (req, res) => {
+
+	const posts = await postSchema.find({});
+	res.send(posts);
+  });
+
+
+  module.exports = router;
