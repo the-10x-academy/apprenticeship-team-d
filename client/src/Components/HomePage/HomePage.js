d3618bdbb054f7f7bea82d20c93eecd2bbdb0422
SaiKiranS <sathpadisaikiran@gmail.com>
2021-02-02 19:13:03 +0530

    added server client connection

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -5,6 +5,20 @@ import PostImage from "../postsPage/postImage";
 import { useState, useEffect } from "react";
 function HomePage(props) {
 	const [userPosts, callUserPosts] = useState(true);
+
+	const [customers, getCustomers] = useState([]); //setting state of customers after getting from server
+
+	//functin for fetching data from server
+	async function callAPI() {
+		const res = await fetch("http://localhost:9000/posts");
+		res.json().then((res) => getCustomers(res));
+	}
+
+	useEffect(() => {
+		callAPI();
+		console.log("called");
+	}, []);
+
 	return userPosts ? (
 		<LandingPage callUserPosts={callUserPosts} />
 	) : (
