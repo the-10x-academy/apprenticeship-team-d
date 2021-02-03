4f721ff0a04ac29c898380b5ecb27fc31f95e936
Shefali <shefali.anand.me@gmail.com>
2021-02-03 19:07:07 +0530

    added minor comments

--- client/src/Components/postsPage/homeHeader.js
+++ client/src/Components/postsPage/homeHeader.js
@@ -7,18 +7,20 @@ export default function Header() {
 	return (
 		<div id="backTopHeader1">
 			<div>
+				{/*header logo and text */}
 				<div className="logoHead">
 					<img className="outer-div" src={icon} alt="l.." />
 					<p className="text1">Instaclone</p>
 				</div>
 				<div className="cam-outer">
-					{/* //url of page to be opened */}
+					{/*url of upload page to be entered*/}
 					<div
 						onClick={() => {
 							console.log("upload Page");
 						}}
 						className="cam-mid"
 					>
+						{/*camera image button opens upload page*/}
 						<img className="camera" src={camera} alt="c.." />
 					</div>
 				</div>
