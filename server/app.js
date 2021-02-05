480ea557949e5ac3b4df4cdff65822ca4182f7a2
SaiKiranS <sathpadisaikiran@gmail.com>
2021-02-05 20:30:23 +0530

    minor changes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-var cors = require("cors");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const posts = require("./index");
